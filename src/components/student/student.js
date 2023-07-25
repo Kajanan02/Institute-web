3ac2b053c48d2d0b043a7d99bf4c7d67f2e3b420
Kajanan02 <kajanan02000@gmail.com>
2023-07-25 16:25:13 +0530

    responsive changes

--- src/components/student/student.js
+++ src/components/student/student.js
@@ -54,7 +54,7 @@ function Students(props) {
                     <div className={"students_container"}>
                         <div><h3 className={"content-heading"}>Students Details</h3></div>
                         <div className={"students-dropdown-container d-flex justify-content-end pb-3"}>
-                            <div className={"marks-dropdown-container"}>
+                            <div className={"table-btn-container"}>
 
 
                                 <button type="button" className={"btn btn-secondary students-dropdown-btn"}
