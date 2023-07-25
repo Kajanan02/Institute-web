c84bd3d23652f329a6cbec0114eb95495e2af631
Kajanan02 <kajanan02000@gmail.com>
2023-07-25 17:18:44 +0530

    changes

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -26,7 +26,7 @@ function Layout({children}) {
 
     return (
         <div className="container-fluid">
-            <div className="row flex-nowrap">
+            <div className="row flex-nowrap overflow-auto">
                 <div
                     className={(!open ? " col-xl-2" : " w-100px") + (!show ? " mobile-navbar-hide " :" mobile-show ") + " col-auto col-md-1 px-0 bg-white border-right min-vh-100 trans"}>
                     <div className={"close-btn-container mobile-hide"} onClick={toggleDrawer}>
