dc80869d675a10ca19ab7de8e6a8ca46594d1cad
Thanujaavks <vksthanujaa@gmail.com>
2023-08-01 17:41:46 +0530

    broadcast home marks changes

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,85 @@
 import React from 'react';
 import Layout from "../../layout/layout";
+import FeatherIcon from 'feather-icons-react';
+
 
 function Home(props) {
     return (
-        <Layout>Home</Layout>
+        <Layout>
+            <div className={"pt-3 ps-3"}>
+                <div className={"row pb-3"}>
+                    <div className={"col-sm-4 mb-3 mb-sm-0"}>
+                        <div className={"card home_card"}>
+                            <div className={"card-body"}>
+                                <h5 className={"card-title"}></h5>
+                                <p className={"card-text"}>Today Income</p>
+                                <p className={"card-text"}>1000000</p>
+                                <p><FeatherIcon className={"action-icons"} icon={"trending-up"} /></p>
+                                <p>this month</p>
+                                <a href="#" className={"btn btn-primary home-btn"}>See Info</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"col-sm-4"}>
+                        <div className={"card home_card"}>
+                            <div className={"card-body"}>
+                                <h5 className={"card-title"}></h5>
+                                <p className={"card-text"}>Pending Income</p>
+                                <p className={"card-text"}>1000000</p>
+                                <p><FeatherIcon className={"action-icons"} icon={"trending-up"} /></p>
+                                <p>this month</p>
+                                <a href="#" className={"btn btn-primary home-btn"}>See Info</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"col-sm-4 mb-3 mb-sm-0"}>
+                        <div className={"card home_card"}>
+                            <div className={"card-body"}>
+                                <h5 className={"card-title"}></h5>
+                                <p className={"card-text"}>Attendance</p>
+                                <p className={"card-text"}>1000000</p>
+                                <p><FeatherIcon className={"action-icons"} icon={"users"} /></p>
+                                <p>this month</p>
+                                <a href="#" className={"btn btn-primary home-btn"}>See Info</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={"col-sm- mb-3 mb-sm-0 pb-3"}>
+                        <div className={"card home_card"}>
+                            <div className={"card-body"}>
+                                <h5 className={"card-title"}></h5>
+                                <p className={"card-text"}></p>
+                                <a href="#" className={"btn btn-primary home-btn"}></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={"row"}>
+                        <div className={"col-sm-6 mb-3 mb-sm-0"}>
+                            <div className={"card home_card"}>
+                                <div className={"card-body"}>
+                                    <h5 className={"card-title"}></h5>
+                                    <p className={"card-text"}></p>
+                                    <a href="#" className={"btn btn-primary"}></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={"col-sm-6"}>
+                            <div className={"card home_card"}>
+                                <div className={"card-body"}>
+                                    <h5 className={"card-title"}></h5>
+                                    <p className={"card-text"}></p>
+                                    <a href="#" className={"btn btn-primary"}></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 }
 
--- src/components/broadcast/brodcast.js
+++ src/components/broadcast/brodcast.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import Layout from "../../layout/layout";
+import broadcast from "../../assets/broadcast.svg";
 
 function Broadcast(props) {
     return (
         <Layout>
-            Brodcast
+            <div className={"ps-3 broadcast_container"}>
+                <div className={"content-heading pt-3"}><h3>Broadcast Message</h3></div>
+
+                <div className={"broadcast_sub_container pt-5"}>
+                    <div className={"broadcast-image pt-4"}>
+                        <img src={broadcast} alt={"broadcast_image"} />
+                    </div>
+                    <div className={"broadcast_msg_container"}>
+                    <div className={"mb-3"}>
+                        <label for="exampleFormControlInput1" className={"form-label message-container"}></label>
+                        <input type="text" className={"form-control lable1"} id="exampleFormControlInput1" placeholder="Message Topic" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" className={"form-label"}></label>
+                        <textarea className={"form-control label1"} id="exampleFormControlTextarea1" placeholder="Message" rows="5"></textarea>
+                    </div>
+                    <p></p>
+                    <div className={"dropdown-center pb-3"}>
+                        <button className={"btn btn-secondary dropdown-toggle send-category-btn"} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Send To
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a className={"dropdown-item"} href="#">
+                                Mathematics</a></li>
+                            <li><a className={"dropdown-item"} href="#">Physics</a></li>
+                            <li><a className={"dropdown-item"} href="#">Chemistry</a></li>
+                        </ul>
+                    </div>
+
+                    <div className={"d-grid gap-2 d-md-block broadcast-send"}>
+                        <button className={"btn btn-primary broadcast-send-btn"} type="button">Send</button>
+                    </div>
+                </div>
+
+                </div>
+            </div>
         </Layout>
     )
 }
