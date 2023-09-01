08afc5b5a20f4bd11202d9f4ab7d7c329f4ae0c6
navaneethan077 <navaneethansivakumaran@gmail.com>
2023-09-01 22:06:39 +0530

    appointment and payment

--- src/components/appointment/appointment.js
+++ src/components/appointment/appointment.js
@@ -1,9 +1,104 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Layout from "../../layout/layout";
+import {appointmentData} from "./damiData";
+import FeatherIcon from 'feather-icons-react';
+import AppointmentForm from "./appointmentForm";
 
 function Appointment(props) {
+    const [appointmentList, setAppointmentList] = useState(appointmentData)
+    const [modalType, setModalType] = useState("view")
+    const [modalShow, setModalShow] = useState(false);
+
+
     return (
-        <Layout>Appointment</Layout>
+        <Layout>
+            <div className={"container"}>
+                <div className={"container-widget"}>
+                    <div className={"students_container"}>
+                        <div><h3 className={"content-heading"}>Appointment</h3></div>
+                        <div className={"students-dropdown-container d-flex justify-content-end pb-3"}>
+                            <div className={"table-btn-container"}>
+
+                                <div className={"appointment-search"}>
+                                    <div className="container-fluid">
+                                        <form className="d-flex" role="search">
+                                            <input className="form-control me-2" type="search" placeholder="Search"
+                                                   aria-label="Search"/>
+                                            <button className="btn btn-outline-success" type="submit">Search</button>
+                                        </form>
+                                    </div>
+                                </div>
+                                <button type="button" className={"btn btn-secondary students-dropdown-btn"}
+                                        onClick={() => {
+                                            setModalType("Add");
+                                            setModalShow(true)
+                                        }}>
+                                    <FeatherIcon className={"action-icons text-white"} icon={"plus"}/>
+                                    Add
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={"table-container p-2 pt-0 "}>
+                        <table className={"table table-hover table-striped sa-table-width"}>
+                            <thead>
+                            <tr className={"position-sticky top-0 pt-1 h-45"}>
+                                <th scope="col">No</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Time</th>
+                                <th scope="col">Parent Name</th>
+                                <th scope="col">Student Name</th>
+                                <th scope="col">Reg.No</th>
+                                <th scope="col">State</th>
+                                <th scope="col"></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {appointmentList.map((data, index) => (<tr key={index + "asd"}>
+                                <th scope="row">{index + 1}</th>
+
+                                <td>{data.date}</td>
+                                <td>{data.Time}</td>
+                                <td>{data.parent_name}</td>
+                                <td>{data.student_name}</td>
+                                <td>{data.Reg}</td>
+                                <td>
+                                    <div className={"appointment_state"}
+                                             onClick={() => {
+
+                                             setModalShow(true)
+                                                 setModalType("State");
+                                         }
+                                    }>{data.state}</div>
+
+                                </td>
+                                <td className={"table-action"}>
+
+                                    <div type="button"
+                                            onClick={() => {
+                                                setModalType("View");
+                                                setModalShow(true)
+                                            }}>
+                                        <FeatherIcon className={"action-icons"} icon={"eye"} onClick={() => setModalType("View")}/>
+
+                                    </div>
+
+
+                                </td>
+                            </tr>))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <AppointmentForm
+                show={modalShow}
+                type={modalType}
+                onHide={() => setModalShow(false)}
+            />
+
+        </Layout>
     );
 }
 
---
+++ src/components/payment-invoice/damiData.js
@@ -0,0 +1,120 @@
+export const paymentData = [{
+    No: 0o1,
+    date: "05 June 2023 ",
+    Time: "09:30",
+    student_name: "Harsh Kadyan",
+    Method: "Debit Cards",
+    Reg: 200012345678,
+    state: "Paid "
+},
+    {
+        No: 0o2,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o3,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o4,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o5,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o6,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o7,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o2,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o3,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o4,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o5,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o6,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    },
+    {
+        No: 0o7,
+        date: "05 June 2023 ",
+        Time: "09:30",
+        student_name: "Harsh Kadyan",
+        Method: "Debit Cards",
+        Reg: 200012345678,
+        state: "Paid "
+    }
+
+]
+
